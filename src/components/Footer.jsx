7b1fc7035f73683a1704906a5fe8f3f4abbdae46
Dekiru <dwyl.agency1@gmail.com>
2026-06-26 15:40:15 +0530

    Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return <div>
+        <div className="absolute flex bottom-0 items-center justify-center w-full bg-black">
+            <h1 className="text-[white]">footer</h1>
+        </div>
+    </div>
+}
+export default Footer
